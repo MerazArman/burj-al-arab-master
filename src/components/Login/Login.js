0205749f461cf00c53303ed3251d5700562153f8
Meraz Arman <merazarman890@gmail.com>
2020-10-15 00:20:00 -0500

    simpoly

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router-dom';
 
 
 const Login = () => {
+    const [loggedUser , setLoggedUser] = useContext(UserContext)
+    const history = useHistory();
+    const location = useLocation();
+    let { from } = location.state || { from: { pathname: "/" } };
+    if(firebase.apps.length===0){
+        firebase.initializeApp(firebaseConfig);
+    }
+   
+    var provider = new firebase.auth.GoogleAuthProvider();
+   const handleGoogleSignIn =()=>{
+    
+   
+    firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
+        // var token = result.credential.accessToken;
+    const {displayName , email} = result.user;
+    const signInUser = {name: displayName , email}
+        setLoggedUser(signInUser)
+        history.replace(from)
+        console.log(signInUser)
+        // ...
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        // ...
+      });
+   }
     return (
         <div>
             <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}> GOOGLE </button>
         </div>
     );
 };
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDa_Cb0ZdWm6HNwgqnabdbRAi8NXrCCO9Q",
+    authDomain: "burj-al-arab-master-15326.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-master-15326.firebaseio.com",
+    projectId: "burj-al-arab-master-15326",
+    storageBucket: "burj-al-arab-master-15326.appspot.com",
+    messagingSenderId: "13256905118",
+    appId: "1:13256905118:web:5e89a1327b21acdd56068f"
+  };
+  export default firebaseConfig;
